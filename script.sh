860f7ddcb799acc2a5c6e44e1812aac3ebf1cd86
Benjamin Du <duchuanlong+git@gmail.com>
2017-05-09 18:41:23 +0800

    use jupyter notebook by default

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-jupyter lab --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
+jupyter notebook --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
+# jupyter lab --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
 
