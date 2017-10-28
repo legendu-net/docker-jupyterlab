f8dbc1d40d3d1c3ac06ff75b18ef22e9ce6f584c
Benjamin Du <duchuanlong+git@gmail.com>
2017-10-28 10:04:06 +0800

    launch jupyterlab rather than jupyter notebook by default

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-jupyter notebook --allow-root --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
-# jupyter lab --allow-root --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
+# jupyter notebook --allow-root --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
+jupyter lab --allow-root --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
 
