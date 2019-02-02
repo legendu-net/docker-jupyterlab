5b8ff3d99690e77c91747108ccf7cfe148789e20
Benjamin Du <duchuanlong+git@gmail.com>
2019-02-02 11:33:51 -0800

    fix the issue on editing settings

--- scripts/04-jupyterlab.sh
+++ scripts/04-jupyterlab.sh
@@ -3,4 +3,5 @@
 # codemirror configuration
 codemirror_dir="$HOME/.jupyter/lab/user-settings/@jupyterlab/codemirror-extension"
 mkdir -p "$codemirror_dir"
-cp /settings/commands.json "$codemirror_dir"
+mv /settings/commands.jupyterlab-settings "$codemirror_dir"
+chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.jupyter
