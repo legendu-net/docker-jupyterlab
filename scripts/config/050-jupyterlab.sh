43ef25ba79b738ad8589be42b1d08efc25f2b679
Benjamin Du <longendu@yahoo.com>
2019-08-17 11:57:34 -0700

    use jupyterlab dark theme by default

--- scripts/config/050-jupyterlab.sh
+++ scripts/config/050-jupyterlab.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # codemirror configuration
-codemirror_dir="$HOME/.jupyter/lab/user-settings/@jupyterlab/codemirror-extension"
-mkdir -p "$codemirror_dir"
-mv /settings/commands.jupyterlab-settings "$codemirror_dir"
+codemirror_dir=$HOME/.jupyter/lab/user-settings/@jupyterlab/codemirror-extension
+mkdir -p $codemirror_dir
+mv /settings/commands.jupyterlab-settings $codemirror_dir
+
+# apputils-extension configuration
+apputils_dir=$HOME/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/
+mkdir -p $apputils_dir
+mv /settings/themes.jupyterlab-settings $apputils_dir
+
 chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.jupyter
