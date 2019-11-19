82529e008137564f84d23635837f6903be860744
Ben Chuanlong Du <legendu.net@outlook.com>
2019-11-19 08:29:05 -0800

    make launch_jupyterlab.sh smarter

--- scripts/launch_jupyterlab.sh
+++ scripts/launch_jupyterlab.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-jupyter lab --allow-root --ip='0.0.0.0' --port=8888 --no-browser --notebook-dir=/workdir
+for f in $(ls /scripts/config/[0-9][0-9][0-9]-*.sh); do
+    source $f
+done
+NOTEBOOK_DIR=/workdir
+if [[ $(id -un) == gitpod ]]; then
+    NOTEBOOK_DIR=/workspace
+fi
+jupyter lab --allow-root --ip='0.0.0.0' --port=8888 --no-browser --notebook-dir=$NOTEBOOK_DIR
