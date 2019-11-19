7f7653e0df225b2aa91f9432744e550f8d1b8cd1
Ben Chuanlong Du <dclong@users.noreply.github.com>
2019-11-19 08:22:18 -0800

    Update 050-jupyterlab.sh

--- scripts/config/050-jupyterlab.sh
+++ scripts/config/050-jupyterlab.sh
@@ -3,11 +3,11 @@
 # codemirror configuration
 codemirror_dir=$HOME/.jupyter/lab/user-settings/@jupyterlab/codemirror-extension
 mkdir -p $codemirror_dir
-mv /settings/commands.jupyterlab-settings $codemirror_dir
+cp /settings/commands.jupyterlab-settings $codemirror_dir
 
 # apputils-extension configuration
 apputils_dir=$HOME/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/
 mkdir -p $apputils_dir
-mv /settings/themes.jupyterlab-settings $apputils_dir
+cp /settings/themes.jupyterlab-settings $apputils_dir
 
 chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.jupyter
