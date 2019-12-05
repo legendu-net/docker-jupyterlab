075b5d18e5223e5eaab12aea2fa823188e05acf6
Ben Chuanlong Du <dclong@users.noreply.github.com>
2019-12-05 14:43:31 -0800

    Update launch_jupyter.sh

--- scripts/launch_jupyter.sh
+++ scripts/launch_jupyter.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export DOCKER_USER=$(id -un)
-/scripts/sys/etc.sh 059
+/scripts/sys/etc.sh pre 059
 NOTEBOOK_DIR=/workdir
 if [[ $(id -un) == gitpod ]]; then
     NOTEBOOK_DIR=/workspace
 fi
 jupyter lab --allow-root --ip='0.0.0.0' --port=8888 --no-browser --notebook-dir=$NOTEBOOK_DIR &
+/scripts/sys/etc.sh post 059
 sleep 5
 /scripts/list_jupyter.py
