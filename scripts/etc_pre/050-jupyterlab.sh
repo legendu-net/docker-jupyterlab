bbcc6cae0d567e5ecf48953282b5cfd693426eb8
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-02-17 23:21:53 -0800

    Update 050-jupyterlab.sh

--- scripts/etc_pre/050-jupyterlab.sh
+++ scripts/etc_pre/050-jupyterlab.sh
@@ -10,4 +10,9 @@ apputils_dir=$HOME/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/
 mkdir -p $apputils_dir
 cp /settings/themes.jupyterlab-settings $apputils_dir
 
+# jupyterlab-lsp
+lsp_dir=/home/dclong/.jupyter/lab/user-settings/@krassowski/jupyterlab-lsp
+mkdir -p $lsp_dir
+cp /settings/jupyterlab-lsp.json $lsp_dir/plugin.jupyterlab-settings
+
 chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.jupyter
