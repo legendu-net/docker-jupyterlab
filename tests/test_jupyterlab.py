017747e21677a53f8a6925fbf3f6062f095f6806
Ben Chuanlong Du <legendu.net@outlook.com>
2021-05-18 23:00:34 -0700

    add testing via pytest

---
+++ tests/test_jupyterlab.py
@@ -0,0 +1,19 @@
+import subprocess as sp 
+
+
+def test_launch():
+    proc = sp.run("""
+        docker run -d --init \
+            --hostname jupyterlab \
+            --log-opt max-size=50m \
+            -p 8888:8888 \
+            -e DOCKER_USER=$(id -un) \
+            -e DOCKER_USER_ID=$(id -u) \
+            -e DOCKER_PASSWORD=$(id -un) \
+            -e DOCKER_GROUP_ID=$(id -g) \
+            -v "$(pwd)":/workdir \
+            -v "$(dirname $HOME)":/home_host \
+            dclong/jupyterlab:next /scripts/sys/init.sh
+            """, shell=True, check=True, capture_output=True)
+    cid = proc.stdout.decode()
+    sp.run(f"docker stop {cid}")
