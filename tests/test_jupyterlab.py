52a93bc5082926e836010476b82b1d33ac75464a
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-05-20 12:40:38 -0700

    Update test_jupyterlab.py

--- tests/test_jupyterlab.py
+++ tests/test_jupyterlab.py
@@ -3,7 +3,7 @@ import subprocess as sp
 
 def test_launch():
     proc = sp.run("""
-        docker run -it --init \
+        docker run -i --init \
             --hostname jupyterlab \
             --log-opt max-size=50m \
             -p 8888:8888 \
