4e4b896918cff3c7442f950331cdb904109c392f
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-05-20 08:56:11 -0700

    Update launch.sh

--- scripts/sys/launch.sh
+++ scripts/sys/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # jupyter notebook --allow-root --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
-su -m $DOCKER_USER -c "jupyter lab --allow-root --ip='0.0.0.0' --port=8888 --no-browser --notebook-dir=/workdir"
+su -m $DOCKER_USER -c "jupyter lab --collaborative --allow-root --ip='0.0.0.0' --port=8888 --no-browser --notebook-dir=/workdir"
 
