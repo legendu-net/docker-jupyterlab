f63eb0ceb4598cc768934f05ade48a1dd168e904
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-05-19 18:31:50 -0700

    Update launch_jupyter.sh

--- scripts/launch_jupyter.sh
+++ scripts/launch_jupyter.sh
@@ -6,7 +6,7 @@ NOTEBOOK_DIR=/workdir
 if [[ $(id -un) == gitpod ]]; then
     NOTEBOOK_DIR=/workspace
 fi
-jupyter lab --collaborative --allow-root --ip='0.0.0.0' --port=8888 --no-browser --notebook-dir=$NOTEBOOK_DIR &
+NIMjupyter lab --collaborative --allow-root --ip='0.0.0.0' --port=8888 --no-browser --notebook-dir=$NOTEBOOK_DIR &
 /scripts/sys/etc.sh post 059
 sleep 5
 /scripts/list_jupyter.py
